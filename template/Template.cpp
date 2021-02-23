691d0a5b417b7d2fd813d02d51ffa36a7f9a2dba
Brice Jacob <bjacob@z4r11p7.42lyon.fr>
2021-02-23 16:07:05 +0100

    ajout template

---
+++ template/Template.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Template_class.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjacob <bjacob@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/23 12:08:04 by bjacob            #+#    #+#             */
+/*   Updated: 2021/02/23 15:24:35 by bjacob           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Template_class.hpp"
+
+Template_class::Template_class(void)
+{
+	return ;
+}
+
+Template_class::~Template_class(void)
+{
+	return ;
+}
---
+++ template/Template.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Template_class.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjacob <bjacob@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/23 12:08:07 by bjacob            #+#    #+#             */
+/*   Updated: 2021/02/23 15:24:13 by bjacob           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEMPLATE_CLASS_HPP
+# define TEMPLATE_CLASS_HPP
+
+#include <string>
+#include <iostream>
+
+class Template_class {
+
+public:
+
+	Template_class(void);
+	~Template_class(void);
+
+private:
+
+
+};
+
+#endif
